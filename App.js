19ce3c5faf55c28346e69024d3e1f27c84018a8f
Niels Koster <n.koster@portavita.eu>
2020-05-23 00:36:48 +0200

    Setup navigation layout

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import AccountScreen from './src/screens/AccountScreen'
+import SigninScreen from './src/screens/SigninScreen'
+import SignupScreen from './src/screens/SignupScreen'
+import TrackListScreen from './src/screens/TrackListScreen'
+import TrackDetailScreen from './src/screens/TrackDetailScreen'
+import TrackCreateScreen from './src/screens/TrackCreateScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    SignupScreen, SigninScreen
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackFlow: createStackNavigator({
+      TrackListScreen, TrackDetailScreen
+    }),
+    TrackCreateScreen, AccountScreen
+  })
+})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(switchNavigator)
