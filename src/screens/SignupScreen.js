9e563b3935f631d7c3b6fe3d242b804288c42f12
Niels Koster <n.koster@portavita.eu>
2020-05-23 17:01:00 +0200

    Use Spacer + more layout stuff

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,20 +1,33 @@
 import React from 'react'
-import { Text, Button } from 'react-native'
+import { Text, Button, Input } from 'react-native-elements'
+import Spacer from '../components/Spacer'
+import { View } from 'react-native'
 
 const SignupScreen = ({ navigation }) => {
     return (
-        <>
-            <Text style={{fontSize:48}}>SignupScreen</Text>
-            <Button
-                title={'signin'}
-                onPress={_ => navigation.navigate('SigninScreen')}
-            />
-            <Button
-                title={'main flow'}
-                onPress={_ => navigation.navigate('mainFlow')}
-            />
-        </>
+        <View style={styles.container}>
+            <Spacer />
+            <Spacer><Text h3>sign up for tracker</Text></Spacer>
+            <Spacer />
+            <Spacer><Input label='email' /></Spacer>
+            <Spacer><Input label='password' /></Spacer>
+            <Spacer><Button title='submit' /></Spacer>
+        </View>
     )
+}
+
+SignupScreen.navigationOptions = _ => {
+    return {
+        headerShown: false
+    }
+}
+
+const styles = {
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 150
+    }
 }
 
 export default SignupScreen
