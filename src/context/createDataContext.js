09b071661ea8dc569cde8df24e055f06b550c69c
Niels Koster <n.koster@portavita.eu>
2020-05-24 01:08:52 +0200

    BUG!! forgot (dispatch)

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -6,7 +6,7 @@ export default (reducer, actions, defaultValue) => {
         const [state, dispatch] = useReducer(reducer, defaultValue)
         const boundActions = {}
         for (let key in actions) {
-            boundActions[key] = actions[key]
+            boundActions[key] = actions[key](dispatch)
         }
         return (
             <Context.Provider value={{ state, ...boundActions }}>
