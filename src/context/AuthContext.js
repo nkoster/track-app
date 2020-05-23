578005afa8f4fedc0713b87b2eef08b445bf42d4
Niels Koster <n.koster@portavita.eu>
2020-05-24 00:56:11 +0200

    /signup, first attempt

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext'
+import trackerApi from '../api/tracker'
 
 const authReducer = (state, action) => {
     switch(action.type) {
@@ -6,8 +7,42 @@ const authReducer = (state, action) => {
     }
 }
 
+// const signup = dispatch => {
+//     console.log('DEBUG')
+//     return async ({ email, password }) => {
+//         console.log('DEBUG II')
+//         try {
+//             const response = await trackerApi.post('/signup', { email, password })
+//             console.log(response)
+//         } catch(err) {
+//             console.log(err.message)
+//         }
+//     }
+// }
+
+const signup = async ({email, password}) => {
+    try {
+        const response = await trackerApi.post('/signup', { email, password })
+        console.log(response)
+    } catch(err) {
+        console.log(err.response.data)
+    }
+}
+
+const signin = dispatch => {
+    return ({ email, password }) => {
+
+    }
+}
+
+const signout = dispatch => {
+    return ({ email, password }) => {
+
+    }
+}
+
 export const { Provider, Context } = createDataContext(
     authReducer,
-    {},
+    { signin, signout, signup },
     { isSignedIn: false }
 )
