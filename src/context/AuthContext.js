1c993884d4c4e0458acb618fa1e0e118496ff883
Niels Koster <n.koster@portavita.eu>
2020-05-24 20:19:31 +0200

    tryLocalSignin dispatcher

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -19,6 +19,16 @@ const clearErrorMessage = dispatch => _ => {
     dispatch({ type: 'clear_error', payload: '' })
 }
 
+const tryLocalSignin = dispatch => async _ => {
+    const token = await AsyncStorage.getItem('token')
+    if (token) {
+        dispatch({ type: 'signin', payload: token })
+        navigate('TrackListScreen', {})
+    } else {
+        navigate('SignupScreen')
+    }
+}
+
 const signup = dispatch => async ({email, password}) => {
     try {
         const response = await trackerApi.post('/signup', { email, password })
@@ -50,6 +60,6 @@ const signout = dispatch => {
 
 export const { Provider, Context } = createDataContext(
     authReducer,
-    { signin, signout, signup, clearErrorMessage },
+    { signin, signout, signup, clearErrorMessage, tryLocalSignin },
     { token: null, errorMessage: '' }
 )
