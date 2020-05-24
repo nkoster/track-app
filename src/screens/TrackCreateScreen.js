59c889912f42502d88833c87b740ea70ddea8c3e
Niels Koster <n.koster@portavita.eu>
2020-05-25 00:11:59 +0200

    startWatching / ask for location services permission

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-navigation'
+import { requestPermissionsAsync } from 'expo-location'
 import { Text } from 'react-native-elements'
 import TrackMap from '../components/TrackMap'
 
 const TrackCreateScreen = _ => {
+    const [err, setErr] = useState(null)
+    const startWatching = async _ => {
+        try {
+            await requestPermissionsAsync()
+        } catch (err) {
+            setErr(err)
+        }
+    }
+    useEffect(_ => { startWatching() }, [])
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h2>Create a Track</Text>
             <TrackMap />
+            { err ? <Text>Please enable location services</Text> : null }
         </SafeAreaView>
     )
 }
