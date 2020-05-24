d425beead6e58c3f710819cd5c64dd3ca8616a0a
Niels Koster <n.koster@portavita.eu>
2020-05-24 20:18:46 +0200

    Use InitialScreen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
+import InitialScreen from './src/screens/InitialScreen'
 import AccountScreen from './src/screens/AccountScreen'
 import SigninScreen from './src/screens/SigninScreen'
 import SignupScreen from './src/screens/SignupScreen'
@@ -12,6 +13,7 @@ import { Provider as AuthProvider } from './src/context/AuthContext'
 import { setNavigator } from './src/navigationRef'
 
 const switchNavigator = createSwitchNavigator({
+  InitialScreen,
   loginFlow: createStackNavigator({
     SignupScreen, SigninScreen
   }),
