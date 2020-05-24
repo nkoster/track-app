5bd00aef9f3d0f6ff24a47a5ef8b75858f042be9
Niels Koster <n.koster@portavita.eu>
2020-05-24 18:09:12 +0200

    Basic sign in layout, copied from SignupScreen and adjusted

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useContext } from 'react'
+import Spacer from '../components/Spacer'
+import { View, StyleSheet } from 'react-native'
+import { Context as AuthContext } from '../context/AuthContext'
+import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink'
 
-const SigninScreen = _ => {
+const SigninScreen = ({ navigation }) => {
+    const { state, signup } = useContext(AuthContext)
+
     return (
-        <View>
-            <Text style={{fontSize:48}}>SigninScreen</Text>
+        <View style={styles.container}>
+            <AuthForm
+                headerText='sign in to tracker'
+                errorMessage={state.errorMessage}
+                onSubmit={_ => {}}
+                submitButtonText='submit'
+            />
+            <Spacer />
+            <NavLink
+                title={`Don't have an account? Click here`}
+                routeName='SignupScreen'
+            />
         </View>
     )
 }
+ SigninScreen.navigationOptions = _ => {
+    return {
+        headerShown: false
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 150
+    },
+    link: {
+        color: 'gray',
+        textAlign: 'center'
+    },
+})
 
 export default SigninScreen
