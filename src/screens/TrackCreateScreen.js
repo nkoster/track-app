5179ff5ca873a6835dcd3816a61b68da5be484cc
Niels Koster <n.koster@portavita.eu>
2020-05-24 22:50:25 +0200

    Use Map component

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-navigation'
+import { Text } from 'react-native-elements'
+import Map from '../components/Map'
 
 const TrackCreateScreen = _ => {
     return (
-        <View>
-            <Text style={{fontSize:48}}>TrackCreateScreen</Text>
-        </View>
+        <SafeAreaView forceInset={{ top: 'always' }}>
+            <Text h2>Create a Track</Text>
+            <Map />
+        </SafeAreaView>
     )
 }
 
