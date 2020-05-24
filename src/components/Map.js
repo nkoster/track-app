a74d1c86779706397545913f07d01d57c93e23bd
Niels Koster <n.koster@portavita.eu>
2020-05-24 22:50:03 +0200

    Map component starter

---
+++ src/components/Map.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native-elements'
+
+const Map = _ => {
+    return (
+        <>
+            <Text>Map</Text>
+        </>
+    )
+}
+
+export default Map
