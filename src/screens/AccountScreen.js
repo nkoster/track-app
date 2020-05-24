58fba967eaa4669b5cf0c194f928a1138c4a3e58
Niels Koster <n.koster@portavita.eu>
2020-05-24 21:15:50 +0200

    Add signout logic

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useContext } from 'react'
+import { Context as AuthContext } from '../context/AuthContext'
+import { Button, Text } from 'react-native-elements'
+import Spacer from '../components/Spacer'
 
 const AccountScreen = _ => {
+    const { signout } = useContext(AuthContext)
     return (
-        <View>
+        <>
             <Text style={{fontSize:48}}>AccountScreen</Text>
-        </View>
+            <Spacer>
+                <Button
+                    title='sign out'
+                    onPress={signout}
+                />
+            </Spacer>
+        </>
     )
 }
 
