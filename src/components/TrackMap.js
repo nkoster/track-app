3ca819e9876205c45662417733c375f69452c8bc
Niels Koster <n.koster@portavita.eu>
2020-05-24 23:26:58 +0200

    Show a map with MapView

--- src/components/TrackMap.js
+++ src/components/TrackMap.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
+import MapView from 'react-native-maps'
 
-const Map = _ => {
+const TrackMap = _ => {
     return (
-        <>
-            <Text>Map</Text>
-        </>
+        <MapView
+            style={styles.map}
+            initialRegion={{
+                latitude: 52.346552,
+                longitude: 5.214206,
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.1
+            }}
+        />
     )
 }
 
-export default Map
+const styles = StyleSheet.create({
+    map: {
+        height: 400
+    }
+})
+
+export default TrackMap
