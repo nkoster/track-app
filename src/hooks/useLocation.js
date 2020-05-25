ded5f696ff60a9485c6b8227cfd6426e1c75b3d6
Niels Koster <n.koster@portavita.eu>
2020-05-26 00:49:00 +0200

    Reusable location hook, extracted from the screen component

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react"
+import {
+    requestPermissionsAsync,
+    watchPositionAsync,
+    Accuracy
+} from 'expo-location'
+
+export default (callback) => {
+    const [err, setErr] = useState(null)
+    const startWatching = async _ => {
+        try {
+            await requestPermissionsAsync()
+            await watchPositionAsync({
+                accuracy: Accuracy.BestForNavigation,
+                timeInterval: 1000,
+                distanceInterval: 10
+            }, callback)
+        } catch (e) {
+            setErr(e)
+        }
+    }
+    useEffect(_ => { startWatching() }, [])
+    return [err]
+}
