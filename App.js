21506522037aacad4fbde91b61753f5e8e9b7e46
Niels Koster <n.koster@portavita.eu>
2020-05-25 21:31:02 +0200

    Use LocationProvider

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import TrackListScreen from './src/screens/TrackListScreen'
 import TrackDetailScreen from './src/screens/TrackDetailScreen'
 import TrackCreateScreen from './src/screens/TrackCreateScreen'
 import { Provider as AuthProvider } from './src/context/AuthContext'
+import { Provider as LocationProvider } from './src/context/LocationContext'
 import { setNavigator } from './src/navigationRef'
 
 const switchNavigator = createSwitchNavigator({
@@ -29,8 +30,10 @@ const App = createAppContainer(switchNavigator)
 
 export default _ => {
   return (
-    <AuthProvider>
-      <App ref={navigator => { setNavigator(navigator) }} />
-    </AuthProvider>
+    <LocationProvider>
+      <AuthProvider>
+        <App ref={navigator => { setNavigator(navigator) }} />
+      </AuthProvider>
+    </LocationProvider>
   )
 }
