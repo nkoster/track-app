1a97a6e15790e13e59228dcc5e080635d1254fe7
Niels Koster <n.koster@portavita.eu>
2020-05-25 23:22:20 +0200

    Make the Map moving

--- src/components/TrackMap.js
+++ src/components/TrackMap.js
@@ -1,36 +1,29 @@
-import React from 'react'
-import { StyleSheet } from 'react-native'
-import { Text } from 'react-native-elements'
+import React, { useContext } from 'react'
+import { StyleSheet, ActivityIndicator } from 'react-native'
 import MapView, { Polyline } from 'react-native-maps'
+import { Context as LocationContext } from '../context/LocationContext'
+import { Accuracy } from 'expo-location'
 
 const TrackMap = _ => {
-    let points = []
-    for (let i=0; i<20; i++) {
-        if (i % 2 === 0) {
-            points.push({
-                latitude: 52.346552 + i * 0.001,
-                longitude: 5.214206 + i * 0.001
-            })
-        } else {
-            points.push({
-                latitude: 52.346552 - i * 0.002,
-                longitude: 5.214206 + i * 0.001
-            })
-        }
+    const { state: { currentLocation } } = useContext(LocationContext)
+    if (!currentLocation) {
+        return <ActivityIndicator size='large' style={{ marginTop: 200 }} />
     }
+    // console.log(currentLocation)
     return (
         <MapView
             style={styles.map}
             initialRegion={{
-                latitude: 52.346552,
-                longitude: 5.214206,
+                ...currentLocation.coords,
                 latitudeDelta: 0.1,
                 longitudeDelta: 0.1
             }}
+            region={{
+                ...currentLocation.coords,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01
+            }}
         >
-            <Polyline
-                coordinates={points}
-            />
         </MapView>
     )
 }
