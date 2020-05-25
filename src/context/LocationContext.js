b9d5537d2feb53037f0f7af86dc5d10f3ab9df32
Niels Koster <n.koster@portavita.eu>
2020-05-25 23:22:49 +0200

    addLocation dispatcher

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -2,13 +2,17 @@ import createDataContext from './createDataContext'
 
 const locationReducer = (state, action) => {
     switch(action.type) {
+        case 'add_location':
+            return { ...state, currentLocation: action.payload }
         default: return state
     }
 }
 
 const startRecording = dispatch => _ => {}
 const stopRecording = dispatch => _ => {}
-const addLocation = dispatch => _ => {}
+const addLocation = dispatch => location => {
+    dispatch({ type: 'add_location', payload: location})
+}
 
 export const { Provider, Context } = createDataContext(
     locationReducer,
