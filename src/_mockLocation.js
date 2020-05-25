783b549f8b5fd56e4f21d47b9868c5c276c90c78
Niels Koster <n.koster@portavita.eu>
2020-05-25 21:31:29 +0200

    almere

--- src/_mockLocation.js
+++ src/_mockLocation.js
@@ -11,8 +11,8 @@ const getLocation = increment => {
             accuracy: 5,
             altitudeAccuracy: 5,
             altitude: 5,
-            longitude: -122.0312186 + increment * tenMetersWithDegrees,
-            latitude: 37.33233141 + increment * tenMetersWithDegrees
+            longitude: 5.2142 + increment * tenMetersWithDegrees,
+            latitude: 52.3465 + increment * tenMetersWithDegrees
         }
     }
 }
