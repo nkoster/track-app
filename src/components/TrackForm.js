27772b65d9744819b6f4ce05cc1c2a51b16bf3a6
Niels Koster <n.koster@portavita.eu>
2020-05-27 00:58:04 +0200

    Use LocationContext / Toggle recording / pull name from Context state

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { Input, Button } from 'react-native-elements'
 import Spacer from './Spacer'
+import { Context as LocationContext } from '../context/LocationContext'
 
 const TrackForm = _ => {
+    const {
+        state: { name, recording },
+        startRecording,
+        stopRecording,
+        changeName
+    } = useContext(LocationContext)
     return (
         <View>
             <Spacer>
             <Input
+                value={_ => name}
+                onChange={changeName}
                 placeholder='please enter a name here'
                 label='TRACK NAME'
             />
             </Spacer>
-            <Button
-                title='start recording track'
-            />
+            {recording
+                ? <Button
+                    title='stop recording track'
+                    onPress={stopRecording}
+                />
+                : <Button
+                    title='start recording track'
+                    onPress={startRecording}
+                />
+            }
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 0
-    }
-})
 
 export default TrackForm
