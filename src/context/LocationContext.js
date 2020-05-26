7f07e463e8e3f1207b32b26e79daf7127bdc805b
Niels Koster <n.koster@portavita.eu>
2020-05-27 00:03:38 +0200

    Dispatcher boilerplate for start/stop recording

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -2,16 +2,35 @@ import createDataContext from './createDataContext'
 
 const locationReducer = (state, action) => {
     switch(action.type) {
-        case 'add_location':
+        case 'add_current_location':
             return { ...state, currentLocation: action.payload }
+        case 'start_recording':
+            return { ...state, recording: true }
+        case 'stop_recording':
+            return { ...state, recording: false }
+        case 'add_location':
+            return { ...state, locations: [...state.locations, action.payload] }
         default: return state
     }
 }
 
-const startRecording = dispatch => _ => {}
-const stopRecording = dispatch => _ => {}
-const addLocation = dispatch => location => {
-    dispatch({ type: 'add_location', payload: location})
+const changeName = dispatch => name => {
+    dispatch({ type: 'change_name', payload: name })
+}
+
+const startRecording = dispatch => _ => {
+    dispatch({ type: 'start_recording' })
+}
+
+const stopRecording = dispatch => _ => {
+    dispatch({ type: 'stop_recording' })
+}
+
+const addLocation = dispatch => (location, recording) => {
+    dispatch({ type: 'add_current_location', payload: location })
+    if (recording) {
+        dispatch({ type: 'add_location', payload: location })
+    }
 }
 
 export const { Provider, Context } = createDataContext(
