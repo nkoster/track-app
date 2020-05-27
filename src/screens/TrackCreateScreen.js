3e254ed90f62230050d6a86432be25fdb136f7a6
Niels Koster <n.koster@portavita.eu>
2020-05-27 22:26:53 +0200

    Fix "stale references" problem with useEffect callback

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,5 +1,5 @@
 import '../_mockLocation'
-import React, { useContext } from 'react'
+import React, { useContext, useCallback } from 'react'
 import { SafeAreaView, withNavigationFocus } from 'react-navigation'
 import { Text } from 'react-native-elements'
 import TrackMap from '../components/TrackMap'
@@ -10,9 +10,10 @@ import Spacer from '../components/Spacer'
 
 const TrackCreateScreen = ({ isFocused }) => {
     const { state, addLocation } = useContext(LocationContext)
-    const [err] = useLocation(isFocused, location => {
+    const callback = useCallback(location => {
         addLocation(location, state.recording)
-    })
+    }, [state.recording])
+    const [err] = useLocation(isFocused, callback)
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h2>Create a Track</Text>
