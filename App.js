7d62cd1889262038bd2f0ed843c68155884d7194
Niels Koster <n.koster@portavita.eu>
2020-05-29 01:24:55 +0200

    Use TrackContext

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ import TrackDetailScreen from './src/screens/TrackDetailScreen'
 import TrackCreateScreen from './src/screens/TrackCreateScreen'
 import { Provider as AuthProvider } from './src/context/AuthContext'
 import { Provider as LocationProvider } from './src/context/LocationContext'
+import { Provider as TrackProvider } from './src/context/TrackContext'
+
 import { setNavigator } from './src/navigationRef'
 
 const switchNavigator = createSwitchNavigator({
@@ -30,10 +32,12 @@ const App = createAppContainer(switchNavigator)
 
 export default _ => {
   return (
-    <LocationProvider>
-      <AuthProvider>
-        <App ref={navigator => { setNavigator(navigator) }} />
-      </AuthProvider>
-    </LocationProvider>
+    <TrackProvider>
+      <LocationProvider>
+        <AuthProvider>
+          <App ref={navigator => { setNavigator(navigator) }} />
+        </AuthProvider>
+      </LocationProvider>
+    </TrackProvider>
   )
 }
