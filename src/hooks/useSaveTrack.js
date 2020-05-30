7908fb8da533d09b85c31fb051a1a26c3ec8eae1
Niels Koster <n.koster@portavita.eu>
2020-05-30 13:00:28 +0200

    Reusable saveTrack hook

--- src/hooks/useSaveTrack.js
+++ src/hooks/useSaveTrack.js
@@ -1,8 +1,13 @@
 import { useContext } from 'react'
-import { Context as LocationContext } from '../context/LocationContext'
 import { Context as TrackContext } from '../context/TrackContext'
+import { Context as LocationContext } from '../context/LocationContext'
 
 export default _ => {
+    const { createTrack } = useContext(TrackContext)
 
+    const { state: { locations, name } } = useContext(LocationContext)
+    const saveTrack = _ => {
+        createTrack(name, locations)
+    }
+    return [saveTrack]
 }
-
