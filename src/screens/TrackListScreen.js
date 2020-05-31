737a46330eba9b007bc07bb06bba6313095f3802
Niels Koster <n.koster@portavita.eu>
2020-06-01 00:02:07 +0200

    fetchTracks and show track list

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import { Text, Button } from 'react-native'
+import React, { useContext } from 'react'
+import { Context as TrackContext } from '../context/TrackContext'
+import { Text, FlatList, TouchableOpacity } from 'react-native'
+import { ListItem } from 'react-native-elements'
+import { NavigationEvents } from 'react-navigation'
 
 const TrackListScreen = ({ navigation }) => {
+    const { state, fetchTracks } = useContext(TrackContext)
+    console.log(state)
     return (
         <>
+            <NavigationEvents
+                onWillFocus={fetchTracks}
+            />
             <Text style={{fontSize:48}}>TrackListScreen</Text>
-            <Button
-                title={'Track Detail'}
-                onPress={_ => navigation.navigate('TrackDetailScreen')}
+            <FlatList
+                data={state}
+                keyExtractor={item => item._id}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity>
+                            <ListItem
+                                chevron
+                                title={item.name}
+                            />
+                        </TouchableOpacity>
+                    )
+                }}
             />
         </>
     )
