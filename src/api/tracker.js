2b50f1c2b2fdc296bb84dd1a26c203294e8e7c74
Niels Koster <n.koster@portavita.eu>
2020-05-31 21:57:53 +0200

    Use API when token available in AsyncStorage

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,22 @@
 import axios from 'axios'
+import { AsyncStorage } from 'react-native'
 
-export default axios.create({
-    baseURL: 'https://255c9b134fd2.ngrok.io'
+const instance = axios.create({
+    baseURL: 'https://c6fb60019e16.ngrok.io'
 })
+
+instance.interceptors.request.use(
+    async config => {
+        const token = await AsyncStorage.getItem('token')
+        // console.log(token)
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    err => {
+        return Promise.reject(err)
+    }
+)
+
+export default instance
