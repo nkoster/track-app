cb7c3d86274799fab09b2b242f60b3378c31b54a
Niels Koster <n.koster@portavita.eu>
2020-06-01 00:27:02 +0200

    Get TrackContext and _id, show track name

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Text, View } from 'react-native'
+import { Context as TrackContext } from '../context/TrackContext'
 
-const TrackDetailScreen = _ => {
+const TrackDetailScreen = ({ navigation }) => {
+    const _id = navigation.getParam('_id')
+    const { state } = useContext(TrackContext)
+    const track = state.find(item => item._id === _id)
     return (
         <View>
-            <Text style={{fontSize:48}}>TrackDetailScreen</Text>
+            <Text style={{fontSize:48}}>{track.name}</Text>
         </View>
     )
 }
