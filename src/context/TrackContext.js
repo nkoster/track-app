b0ca40a391d3da5787d12015e37cc350cb09ff77
Niels Koster <n.koster@portavita.eu>
2020-06-01 00:01:31 +0200

    fetchTracks get / dispatcher

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -3,12 +3,17 @@ import trackerApi from '../api/tracker'
 
 const trackReducer = (state, action) => {
     switch(action.type) {
+        case 'fetchTracks':
+            return action.payload
         default:
             return state
     }
 }
 
-const fetchTracks = dispatch => _ => {}
+const fetchTracks = dispatch => async _ => {
+    const response = await trackerApi.get('/tracks')
+    dispatch({ type: 'fetchTracks', payload: response.data })
+}
 
 const createTrack = dispatch => async (name, locations) => {
     await trackerApi.post('/track', { name, locations })
