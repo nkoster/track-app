f96df7e152790dfc0fecf5c46ae99a3be5e83068
Niels Koster <n.koster@portavita.eu>
2020-06-01 00:46:16 +0200

    Show map and track polyline

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,16 +1,35 @@
 import React, { useContext } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import { Context as TrackContext } from '../context/TrackContext'
+import MapView, { Polyline } from 'react-native-maps'
 
 const TrackDetailScreen = ({ navigation }) => {
     const _id = navigation.getParam('_id')
     const { state } = useContext(TrackContext)
     const track = state.find(item => item._id === _id)
+    const initialCoords = track.locations[0].coords
     return (
         <View>
             <Text style={{fontSize:48}}>{track.name}</Text>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    longitudeDelta: 0.01,
+                    latitudeDelta: 0.01,
+                    ...initialCoords
+                }}
+            >
+                <Polyline
+                    coordinates={track.locations.map(location => location.coords)}
+                />
+            </MapView>
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    map: {
+        height: 300
+    }
+})
 export default TrackDetailScreen
