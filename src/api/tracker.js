7241c5c184554ccafd4cf9c58cc06679877a544c
Niels Koster <n.koster@portavita.eu>
2020-06-01 22:23:19 +0200

    ngrok

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { AsyncStorage } from 'react-native'
 
 const instance = axios.create({
-    baseURL: 'https://ac5d1d9bbeeb.ngrok.io'
+    baseURL: 'https://a2920eed316e.ngrok.io'
 })
 
 instance.interceptors.request.use(
