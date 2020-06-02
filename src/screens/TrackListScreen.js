b8da670d15d56c56bc239cd11bc6c34602f4cabc
Niels Koster <n.koster@portavita.eu>
2020-06-03 01:59:01 +0200

    Show timestamps

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,25 +1,34 @@
 import React, { useContext } from 'react'
 import { Context as TrackContext } from '../context/TrackContext'
-import { FlatList, TouchableOpacity } from 'react-native'
-import { ListItem } from 'react-native-elements'
+import { FlatList, TouchableOpacity, StyleSheet } from 'react-native'
+import { ListItem, Text } from 'react-native-elements'
 import { NavigationEvents } from 'react-navigation'
 
 const TrackListScreen = ({ navigation }) => {
     const { state, fetchTracks } = useContext(TrackContext)
+    // const id = state[0] ? state[0]._id : ''
+    const getDate = id => {
+        const timestamp = id.substring(0,8)
+        const date = new Date(parseInt(timestamp, 16) * 1000)
+        return date.toDateString
+    }
     return (
         <>
             <NavigationEvents
                 onWillFocus={fetchTracks}
             />
             <FlatList
-                data={state}
+                data={state.slice().reverse()}
                 keyExtractor={item => item._id}
                 renderItem={({ item }) => {
                     return (
                         <TouchableOpacity onPress={_ => {
                             navigation.navigate('TrackDetailScreen', { _id: item._id })
                         }} >
-                            <ListItem chevron title={item.name} />
+                            <Text style={styles.list}>{
+                                (new Date(parseInt(item._id.substring(0,8), 16) * 1000)).toString()
+                            }</Text>
+                            <ListItem chevron title={`${item.name}`} />
                         </TouchableOpacity>
                     )
                 }}
@@ -31,5 +40,14 @@ const TrackListScreen = ({ navigation }) => {
 TrackListScreen.navigationOptions = {
     title: 'Tracks'
 }
+
+const styles = StyleSheet.create({
+    list: {
+        backgroundColor: 'white',
+        marginTop: 10,
+        paddingLeft: 6,
+        paddingTop: 3
+    }
+})
 
 export default TrackListScreen
