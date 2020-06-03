e2f4da3e84bd4ce203c18fb54f6f354b22dbd4f9
Niels Koster <n.koster@portavita.eu>
2020-06-03 21:57:40 +0200

    cleanup style bullshit

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View } from 'react-native'
 import { Input, Button } from 'react-native-elements'
 import { Context as LocationContext } from '../context/LocationContext'
 import useSaveTrack from '../hooks/useSaveTrack'
@@ -15,7 +15,6 @@ const TrackForm = _ => {
     return (
         <View>
             <Input
-                style={styles.input}
                 value={name}
                 onChangeText={changeName}
                 placeholder='enter a name here'
@@ -26,18 +25,15 @@ const TrackForm = _ => {
                 name
                 ?   recording
                     ? <Button
-                        style={styles.button}
                         buttonStyle={{backgroundColor:'#e47'}}
                         title='stop tracker'
                         onPress={stopRecording} />
                     : !recording && locations.length && name
                         ? <Button
-                            style={styles.button}
                             buttonStyle={{backgroundColor:'#496'}}
                             title={'save this track'}
                             onPress={saveTrack} />
                         : <Button
-                            style={styles.button}
                             buttonStyle={{backgroundColor:'#5090ff'}}
                             title='start tracker'
                             onPress={startRecording} />
@@ -47,17 +43,5 @@ const TrackForm = _ => {
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        // flexDirection: 'column'
-    },
-    button: {
-        // paddingHorizontal: 12,
-        // marginBottom: 6
-    },
-    input: {
-    }
-})
 
 export default TrackForm
