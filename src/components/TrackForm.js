33ae6d512cbf7ae4f39906b9b5a9d4283a4a5892
Niels Koster <n.koster@portavita.eu>
2020-06-04 00:59:37 +0200

    cleanup

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { View } from 'react-native'
 import { Input, Button } from 'react-native-elements'
 import { Context as LocationContext } from '../context/LocationContext'
