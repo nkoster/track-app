6d663c44dc7e3dfc18a2958ed288ec87721cf89f
Niels Koster <n.koster@portavita.eu>
2020-06-05 16:25:20 +0200

    attempt to give saving feedback

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react'
-import { View } from 'react-native'
+import React, { useContext, useState, useEffect } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 import { Input, Button } from 'react-native-elements'
 import { Context as LocationContext } from '../context/LocationContext'
 import useSaveTrack from '../hooks/useSaveTrack'
@@ -12,6 +12,10 @@ const TrackForm = _ => {
         changeName
     } = useContext(LocationContext)
     const [saveTrack] = useSaveTrack()
+    const [savePressed, setSavePressed] = useState(false)
+    useEffect(_ => {
+        setSavePressed(false)
+    })
     return (
         <View>
             <Input
@@ -30,17 +34,30 @@ const TrackForm = _ => {
                     : !recording && locations.length && name
                         ? <Button
                             buttonStyle={{backgroundColor:'#496'}}
-                            title={'save this track'}
-                            onPress={saveTrack} />
+                            title={savePressed ? 'saving, please wait...' : 'save this track'}
+                            onPress={async data => {
+                                setSavePressed(true)
+                                await saveTrack(data)}} />
                         : <Button
                             buttonStyle={{backgroundColor:'#5090ff'}}
                             title='start tracker'
-                            onPress={startRecording} />
+                            onPress={data => {
+                                startRecording(data)
+                            }} />
                 : null
             }
+            { savePressed ? <Text style={styles.wait}>saving data...</Text> : null }
             </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    wait: {
+        textAlign: 'center',
+        padding: 20,
+        fontSize: 16
+    }
+})
 
 export default TrackForm
