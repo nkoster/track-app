6f3436d5548c69fffc93e5509841f1baaf1ac357
Niels Koster <n.koster@portavita.eu>
2020-06-07 21:11:56 +0200

    spaces instead of comma

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -23,8 +23,8 @@ const TrackListScreen = ({ navigation }) => {
                             }</Text>
                             <ListItem
                                 chevron
-                                title={`${item.name}, ${(item.distance/1000).toFixed(3)} km, ${(item.duration/60).toFixed(1)} min`}
-                                titleStyle={{fontWeight: 'bold', color: '#444'}}
+                                title={`${item.name}    ${(item.distance/1000).toFixed(3)} km    ${(item.duration/60).toFixed(1)} min`}
+                                titleStyle={{fontWeight: 'bold', fontSize: 16, color: '#444'}}
                             />
                         </TouchableOpacity>
                     )
